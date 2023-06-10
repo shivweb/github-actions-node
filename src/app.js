69c30665493b8507e45b0e92a2eef761c850435c
shivweb <sp10595@gmail.com>
2023-06-10 15:30:29 +0530

    push github actions for node

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/testNode', (req, res) => {
+    res.status(200).send("Yes the testNode endpoint worked")
+});
+
+module.exports = app;
