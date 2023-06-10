b3943a30993c291e720d505482d2dcd06e544d2c
shivweb <sp10595@gmail.com>
2023-06-10 17:09:58 +0530

    updated code for nodetest

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/testNode', (req, res) => {
-    res.status(200).send("Yes the testNode endpoint worked")
+    res.status(200).send("Yes the testNode endpoint worked again")
 });
 
 module.exports = app;
